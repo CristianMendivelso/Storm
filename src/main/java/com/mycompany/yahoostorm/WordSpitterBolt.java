e8715eb7bdb63d31be69d9739dbbe70adfc3a653
cristianmendivelso <cristian.mendivelso@mail.escuelaing.edu.co>
2017-06-17 16:20:41 -0500

    Word count

---
+++ src/main/java/com/mycompany/yahoostorm/WordSpitterBolt.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.yahoostorm;
+import java.util.Map;
+
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.IRichBolt;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+
+public class WordSpitterBolt implements IRichBolt{
+	private OutputCollector collector;
+	@Override
+	public void prepare(Map stormConf, TopologyContext context,
+			OutputCollector collector) {
+		this.collector = collector;
+	}
+	@Override
+	public void execute(Tuple input) {
+		String sentence = input.getString(0);
+		String[] words = sentence.split(" ");
+		for(String word: words){
+			word = word.trim();
+			if(!word.isEmpty()){
+				word = word.toLowerCase();
+				collector.emit(new Values(word));
+			}
+		}
+		collector.ack(input);
+	}
+	@Override
+	public void declareOutputFields(OutputFieldsDeclarer declarer) {
+		declarer.declare(new Fields("word"));
+	}
+
+	@Override
+	public void cleanup() {
+	}
+	@Override
+	public Map<String, Object> getComponentConfiguration() {
+		return null;
+	}
+}
